2168bb49783a18d146d4de3d4ef30e765bec906f
ltbringer <amresh.venugopal@gmail.com>
2023-03-08 01:55:15 +0530

    add: instantiation of SSTables.

--- src/sstable/sstable.rs
+++ src/sstable/sstable.rs
@@ -8,3 +8,26 @@ pub struct SSTable {
     filename: PathBuf,
     file: File
 }
+
+impl SSTable {
+    /**
+     * The anatomy of an SSTable:
+     * 
+     * |0|0|0|0|0|0|0|9|t|e|s|t|_|m|o|d|e|0|0|0|0|0|0|0|7|1|2|3|4|5|6|7|
+     * |<- Key length->|<-key contents-->|<- Val length->|<-- Value -->|
+     * |0|0|0|0|0|0|0|4|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_
+     * |<- Key length->| ...
+     * 
+     * Notice: the key `test_mode` is 9 characters long. That's what the 
+     * `Key length` is trying to specify. The same explains the following
+     * `Val length`. 
+     */
+    pub fn new(filename: PathBuf) -> io::Result<SSTable> {
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(filename.clone())?;
+
+        Ok(SSTable { filename, file })
+    }
