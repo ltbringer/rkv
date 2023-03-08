fb54376de407fff9cb5809fa9150cab8637afc2b
ltbringer <amresh.venugopal@gmail.com>
2023-03-08 23:57:23 +0530

    test: remove panic on overflow.

--- src/store/store_test.rs
+++ src/store/store_test.rs
@@ -1,26 +1,26 @@
 #[cfg(test)]
 mod store_test {
+    use std::panic::{self, AssertUnwindSafe};
+    use tempfile::TempDir;
     use super::super::store::KVStore;
 
     #[test]
     fn test_add_item() {
         let key = b"life";
         let value = b"42";
-
-        let mut store = KVStore::new(None);
-        store.set(key, value);
-        match store.get(b"life") {
-            Some(v) => assert_eq!(v, value, "Expected value to be b'42'"),
-            None => panic!("Expected value to be b'42'")
-        }
-    }
-
-    #[test]
-    #[should_panic(expected = "Size overflow, max-size=10, current-size=11")]
-    fn test_overflow_panic() {
-        let key = b"12345";
-        let value = b"678910";
-        let mut store = KVStore::new(Some(10));
-        store.set(key, value);
+        let result = panic::catch_unwind(AssertUnwindSafe(|| {
+            let temp_dir = match TempDir::new() {
+                Ok(dir) => dir,
+                Err(_) => panic!("Failed creating tempdir.")
+            };
+            let mut store = KVStore::new(20, temp_dir.into_path());
+            store.set(key, value);
+            match store.get(b"life") {
+                Some(v) => assert_eq!(v, value, "Expected value to be b'42'"),
+                None => panic!("Expected value to be b'42'")
+            }
+            drop(store);
+        }));
+        assert!(result.is_ok());
     }
 }
