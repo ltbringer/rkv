6fa5325ebd75b2adae8de7a7afb055b12d78e12c
ltbringer <amresh.venugopal@gmail.com>
2023-03-11 15:58:23 +0530

    test: key reads from sstable.

--- src/store/store_test.rs
+++ src/store/store_test.rs
@@ -23,4 +23,33 @@ mod store_test {
         }));
         assert!(result.is_ok());
     }
+
+    #[test]
+    fn test_sstable_read() {
+        let setup = [(b"key1", b"value1"), 
+            (b"key2", b"value2"), 
+            (b"key3", b"value3"), 
+            (b"key4", b"value4"), 
+            (b"key5", b"value5"), 
+            (b"key6", b"value6"), 
+            (b"key7", b"value7")];
+
+        let result = panic::catch_unwind(AssertUnwindSafe(|| {
+            let temp_dir = match TempDir::new() {
+                Ok(dir) => dir,
+                Err(_) => panic!("Failed creating tempdir.")
+            };
+            let mut store = KVStore::new(5, temp_dir.into_path());
+            for (key , value) in setup {
+                store.set(key, value);
+            }
+
+            match store.get(b"key1") {
+                Some(v) => assert_eq!(v, b"value1", "Expected value to be b'value1'"),
+                None => panic!("Expected a value to be found'")
+            }
+            drop(store);
+        }));
+        assert!(result.is_ok());
+    }
 }
