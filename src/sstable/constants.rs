1c42f218e482bef893bc5645006949258d1828fd
ltbringer <amresh.venugopal@gmail.com>
2023-03-22 02:56:36 +0530

    update: create word sizes for keys and values separately

--- src/sstable/constants.rs
+++ src/sstable/constants.rs
@@ -1,3 +1,5 @@
 pub static WORD: usize = 8;
+pub static KEY_WORD: usize = WORD;
+pub static VALUE_WORD: usize = WORD;
 pub static TOMBSTONE: &[u8] = &[];
-pub static RKV: &str = ".rkv";
+pub static RKV: &str = "rkv";
