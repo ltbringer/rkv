011cbddf359fb830795d7698d179a77300ad6f9a
ltbringer <amresh.venugopal@wise.com>
2023-03-25 05:03:34 +0000

    benchmark: 1M keys.
    latency: (150 - 220) us.
    throughput: 73 - 109 KiB/s

--- benches/store_benchmark.rs
+++ benches/store_benchmark.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Through
 use rkv::store::lsm_store::KVStore;
 use tempfile::TempDir;
 
-fn as_bytes(x: Vec<u32>) -> Vec<Vec<u8>> {
+fn as_bytes(x: Vec<u64>) -> Vec<Vec<u8>> {
     x.iter()
         .map(|k| k.to_be_bytes().to_vec())
         .collect::<Vec<Vec<u8>>>()
@@ -13,27 +13,27 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         Ok(dir) => dir,
         Err(_) => panic!("Failed creating tempdir."),
     };
-    let mut store = KVStore::new(100, temp_dir.into_path());
     let n_keys = 1_000_000;
-    let keys: Vec<u32> = (0..n_keys).collect();
-    let values: Vec<u32> = (0..n_keys).collect();
+    let size = 16;
+    let key_per_table = 100_000 * size;
+    let mut store = KVStore::new(key_per_table, temp_dir.into_path());
+    let step = (n_keys / 5) as usize;
+    let keys = (0..n_keys).collect::<Vec<u64>>();
     let key_bytes = as_bytes(keys);
-    let value_bytes = as_bytes(values);
-    let step = (n_keys / 10) as usize;
 
-    let mut group = c.benchmark_group(format!("store.get for {} keys", n_keys));
-    group.significance_level(0.1).sample_size(10);
-    for (k, v) in key_bytes.iter().zip(value_bytes.iter()) {
-        store.set(k, v);
+    for k in key_bytes.iter() {
+        store.set(k, k);
     }
 
-    for (i, (k, v)) in key_bytes
-        .iter()
-        .zip(value_bytes.iter())
-        .step_by(step)
-        .enumerate()
-    {
-        let size = k.len() + v.len();
+    let mut group = c.benchmark_group(format!(
+        "store.get-for-{}-keys-in-{}-sstable(s)",
+        n_keys,
+        (n_keys * size) / key_per_table
+    ));
+    group.significance_level(0.1).sample_size(10);
+
+    for (i, k) in key_bytes.iter().step_by(step).enumerate() {
+        let size = k.len() + k.len();
         group.throughput(Throughput::Bytes(size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(i), k, |b, k| {
             b.iter(|| store.get(k))
