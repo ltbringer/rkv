0f2c2c41e2278c729f23c14c9942cb717ea969fe
ltbringer <amresh.venugopal@gmail.com>
2023-04-05 02:57:25 +0530

    update: target contains the html reports.

--- scripts/cloud_bench.sh
+++ scripts/cloud_bench.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 S3_URI=$1
-mkdir -p /tmp/bench-reports
-cargo bench --target-dir /tmp/bench-reports
-aws s3 cp /tmp/bench-reports $S3_URI --recursive
+cargo bench
+aws s3 cp ../target/criterion $S3_URI --recursive
