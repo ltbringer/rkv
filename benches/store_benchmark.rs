62ab6a30dc0e2d1cc9a2f00f748e53f6d17ab5e1
ltbringer <amresh.venugopal@gmail.com>
2023-04-09 14:58:14 +0530

    update: insert k-v faster using threads.

--- benches/store_benchmark.rs
+++ benches/store_benchmark.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Through
 use rand::{distributions::Alphanumeric, Rng};
 use rkv::store::lsm_store::KVStore;
 use std::env;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use tempfile::tempdir;
 
@@ -26,39 +26,68 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     };
 
     let key_length = match env::var("KEY_LENGTH") {
-        Ok(env_n_keys) => env_n_keys.parse().unwrap_or(500),
+        Ok(key_length) => key_length.parse().unwrap_or(500),
         Err(_) => 500,
     }; // Max 65535
-    let key_per_table = 100_000;
+
+    let max_threads = num_cpus::get();
+
+    let n_threads: usize = match env::var("THREADS") {
+        Ok(n_threads) => n_threads.parse().unwrap_or(max_threads),
+        Err(_) => max_threads,
+    };
+
+    let n_threads = n_threads.clamp(1, max_threads);
+    let key_per_table = 50000;
     let mut store = KVStore::new("benchmark".to_owned(), key_per_table, path.clone());
-    let step = (n_keys / 5) as usize;
-
-    println!("Creating {} random key strings.", n_keys);
-
-    let keys = (0..n_keys)
-        .map(|_| rand_string(key_length))
-        .collect::<Vec<String>>();
-    let key_chunks: Vec<Vec<String>> = keys.chunks(8).map(|c| c.to_vec()).collect();
+    let step = n_keys / 5;
 
     println!(
         "PARAMS:
-        , n_keys={}
-        , key_length={}
-        , key_per_table={} (before compaction)
-        , step={}",
-        n_keys, key_length, key_per_table, step
+        , n_keys={n_keys}
+        , n_threads={n_threads}
+        , max_threads={max_threads}
+        , key_length={key_length}
+        , step={step}
+        , key_per_table={key_per_table} (before compaction)",
+        n_keys = n_keys,
+        n_threads = n_threads,
+        max_threads = max_threads,
+        key_length = key_length,
+        key_per_table = key_per_table,
+        step = step
     );
 
     println!("Inserting {} keys ...", n_keys);
 
     let mut thread_handlers = vec![];
+    let test_keys: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![]));
+    let ctr: Arc<Mutex<u64>> = Arc::new(Mutex::new(0));
 
-    for k in key_chunks.iter() {
-        let chunk_ref = Arc::new(k.clone());
-        let mut store_ref = store.clone();
+    for chunk in 0..n_threads {
+        let test_keys = test_keys.clone();
+        let mut store = store.clone();
+        let ctr = ctr.clone();
         let handle = thread::spawn(move || {
-            for k in chunk_ref.iter() {
-                store_ref.set(k.as_bytes(), k.as_bytes());
+            let start = chunk * (n_keys / n_threads);
+            let end = start + (n_keys / n_threads);
+
+            for i in start..end {
+                let k = rand_string(key_length);
+                if i % step == 0  {
+                    match test_keys.lock() {
+                        Ok(mut test_keys) => test_keys.push(k.clone()),
+                        Err(e) => panic!("Poisoned lock: {:?}", e),
+                    }
+                }
+                store.set(k.as_bytes(), k.as_bytes());
+                match ctr.lock() {
+                    Ok(mut ctr) => {
+                        *ctr += 1;
+                        println!("Inserted key: ({}/{})", ctr, n_keys);
+                    },
+                    Err(e) => panic!("Poisoned lock: {:?}", e),
+                }
             }
         });
         thread_handlers.push(handle);
@@ -76,14 +105,19 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         n_keys, key_length
     ));
 
-    group.significance_level(0.1).sample_size(10);
+    group.significance_level(0.1).sample_size(50);
 
-    for (i, k) in keys.iter().step_by(step).enumerate() {
-        let size = k.len() + k.len();
-        group.throughput(Throughput::Bytes(size as u64));
-        group.bench_with_input(BenchmarkId::from_parameter(i), k, |b, k| {
-            b.iter(|| store.get(k.as_bytes()))
-        });
+    match test_keys.lock() {
+        Ok(test_keys) => {
+            for (i, k) in test_keys.iter().enumerate() {
+                let size = k.len() + k.len();
+                group.throughput(Throughput::Bytes(size as u64));
+                group.bench_with_input(BenchmarkId::from_parameter(i), k, |b, k| {
+                    b.iter(|| store.get(k.as_bytes()))
+                });
+            }        
+        },
+        Err(e) => panic!("Poisoned lock: {:?}", e),
     }
 
     group.finish();
